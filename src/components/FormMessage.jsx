4770a85b4ef7cb1bd8c0efde8feecf837063d23b
Jhonjandrys <jhonjandrysramirez10@gmail.com>
2025-01-08 21:07:52 -0500

    fix: edit colors for theme dark and theme white

--- src/components/FormMessage.jsx
+++ src/components/FormMessage.jsx
@@ -29,7 +29,7 @@ const ContactForm = () => {
     <>
       <section>
         <div className="py-8 lg:py-14 px-4 mx-auto max-w-screen-md">
-          <p class="mb-8 lg:mb-16 font-semibold text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+          <p class="mb-8 lg:mb-16 font-semibold text-center text-gray-400 sm:text-xl">
             No dudes en ponerte en contacto conmigo a través de este formulario
             o directamente a mi correo electrónico para cualquier proyecto o
             asesoria en la idea de tu pagina web.
@@ -46,7 +46,7 @@ const ContactForm = () => {
             <div>
               <label
                 htmlFor="name"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                className="block mb-2 text-sm font-medium  text-gray-300"
               >
                 Nombre
               </label>
@@ -54,7 +54,7 @@ const ContactForm = () => {
                 type="text"
                 id="name"
                 name="name"
-                className="block p-2.5 w-full text-sm rounded-lg shadow-sm border border-white focus:ring-primary-500 focus:border-primary-500 dark:bg-white/15 dark:border-white/40 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                className="block p-2.5 w-full text-sm rounded-lg shadow-sm border border-white focus:ring-primary-500 focus:border-primary-500 bg-white/15 border-white/40 dark:placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500"
                 placeholder="Escribe tu nombre"
                 required
               />
@@ -62,7 +62,7 @@ const ContactForm = () => {
             <div>
               <label
                 htmlFor="email"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                className="block mb-2 text-sm font-medium text-gray-300"
               >
                 Correo Electrónico
               </label>
@@ -70,7 +70,7 @@ const ContactForm = () => {
                 type="email"
                 id="email"
                 name="email"
-                className="block p-2.5 w-full text-sm rounded-lg shadow-sm border border-white focus:ring-primary-500 focus:border-primary-500 dark:bg-white/15 dark:border-white/40 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                className="block p-2.5 w-full text-sm rounded-lg shadow-sm border border-white focus:ring-primary-500 focus:border-primary-500 bg-white/15 border-white/40 dark:placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500"
                 placeholder="Escribe tu correo electronico"
                 required
               />
@@ -78,7 +78,7 @@ const ContactForm = () => {
             <div className="sm:col-span-2">
               <label
                 htmlFor="message"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+                className="block mb-2 text-sm font-medium text-gray-300"
               >
                 Mensaje
               </label>
@@ -86,13 +86,13 @@ const ContactForm = () => {
                 id="message"
                 rows="6"
                 name="message"
-                className="block p-2.5 w-full text-sm rounded-lg shadow-sm border border-white focus:ring-primary-500 focus:border-primary-500 dark:bg-white/15 dark:border-white/40 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                className="block p-2.5 w-full text-sm rounded-lg shadow-sm border border-white focus:ring-primary-500 focus:border-primary-500 bg-white/15 border-white/40 dark:placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500"
                 placeholder="Escribe tu mensaje"
               ></textarea>
             </div>
             <button
               type="submit"
-              className="border rounded-full inline-flex gap-x-2 py-2 justify-center items-center px-3 text-sm font-medium hover:bg-gray-100 dark:[background-color:#2f6fe3] dark:border-white hover:font-bold dark:hover:[background-color:#2815c3] dark:text-white hover:scale-110 transition"
+              className="border rounded-full inline-flex gap-x-2 py-2 justify-center items-center px-3 text-sm font-medium hover:bg-gray-100 [background-color:#2f6fe3] border-white hover:font-bold hover:[background-color:#2815c3] text-white hover:scale-110 transition"
             >
               Enviar mensaje
             </button>
